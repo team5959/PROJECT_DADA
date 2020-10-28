63ecf8db20284f0f5e234f468fdc97cedcc9d028
김종관 <colinder@naver.com>
2020-10-28 16:36:36 +0900

    :new: / ADD typescript and navigate apply /

---
+++ frontend/src/Components/user.tsx
@@ -0,0 +1,29 @@
+import 'react-native-gesture-handler';
+import React from 'react'
+import { Text, View, Button } from 'react-native'
+
+
+const UserScreen = ({ route, navigation }) => {
+    const { userIdx, userName, userLastName } = route.params; //전달받은 값(route)
+
+    return (
+        <View style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <Text>User Screen</Text>
+            <Button 
+                title="To Home Screen"
+                onPress={() => {
+                    navigation.navigate('Home')
+                }}
+            />
+            <Text>User Idx: {JSON.stringify(userIdx)}</Text>
+            <Text>User Name: {JSON.stringify(userName)}</Text>
+            { route.params.userLastName && <Text>User LastName: {JSON.stringify(userLastName)}</Text>}
+        </View>
+    )
+}
+
+export default UserScreen
