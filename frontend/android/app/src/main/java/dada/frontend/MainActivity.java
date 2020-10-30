49514d891265ee7bb4ad3e5c0b479e35c2b02ade
김종관 <colinder@naver.com>
2020-10-30 15:26:25 +0900

    :new: / ADD googlr login / S03P31A509-17

--- frontend/android/app/src/main/java/dada/frontend/MainActivity.java
+++ frontend/android/app/src/main/java/dada/frontend/MainActivity.java
@@ -1,4 +1,4 @@
-package com.frontend;
+package dada.frontend;
 
 import com.facebook.react.ReactActivity;
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,24 @@
 import 'react-native-gesture-handler';
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import HomeScreen from './Components/home'
-import UserScreen from './Components/user'
+import LoginScreen from './Components/Login'
+import MainScreen from './Components/main'
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [isLoggedIn, setisLoggedIn] = useState(false)
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen
-          name='User'
-          component={UserScreen}
-        />
+      <Stack.Navigator>
+        {isLoggedIn ? (
+          <Stack.Screen name='Main' component={MainScreen} options={{ headerShown: false }}/>
+        ) : (
+          <Stack.Screen name='Login' component={LoginScreen} options={{ headerShown: false }}/>
+        )}         
       </Stack.Navigator>
     </NavigationContainer>
   )
