e294f206407b23b2cc06b83d03ac429cb7b5dd7b
Hyejin Park <triaina0@gmail.com>
2020-10-30 18:26:24 +0900

    :new: / Add createPeed / S03P31A509-18

---
+++ sls-dada/peed/create.ts
@@ -0,0 +1,47 @@
+'use strict';
+
+import * as uuid from 'uuid';
+import {DynamoDB} from 'aws-sdk';
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+    const data = JSON.parse(event.body);
+
+    const tags = dynamoDb.createSet(data.tags);
+    delete data['tags'];
+
+    const photos = data.photos;
+    photos.forEach(item => {
+        item.tags = dynamoDb.createSet(item.tags);
+    })
+    delete data['photos'];
+
+    const params = {
+        TableName: 'peed',
+        Item: {
+            id: uuid.v4(),
+            tags: tags,
+            photos: photos,
+            ...data,
+        }
+    };
+
+    dynamoDb.put(params, (error, result) => {
+        if (error) {
+            console.error(error);
+            callback(null, {
+                statusCode: error.statusCode || 501,
+                headers: {'Content-Type': 'text/plain'},
+                body: 'Couldn\'t fetch the todo items.',
+            });
+            return;
+        }
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(params.Item),
+        };
+        callback(null, response);
+    });
+};
