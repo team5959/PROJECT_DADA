201d80d465233ad29f589d81900d8ebb3a8f01e3
김종관 <colinder@naver.com>
2020-11-02 17:28:41 +0900

    :new: / login && main page setting / S03P31A509-14

--- frontend/src/Components/Screens/home.tsx
+++ frontend/src/Components/Screens/home.tsx
@@ -1,6 +1,8 @@
 import 'react-native-gesture-handler';
 import React from 'react'
 import { Text, View, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 
 const HomeScreen = ({ navigation }) => {
--- frontend/src/Components/main.tsx
+++ frontend/src/Components/main.tsx
@@ -1,12 +1,36 @@
+
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './Screens/home'
+import UserScreen from './Screens/user'
+
+const Tab = createBottomTabNavigator();
 
 const main = () => {
   return (
-    <View>
-      <Text>main page</Text>
-    </View>
+    // <View style={styles.title}>
+    //   <Text >메인 화면 왔어요</Text>
+    // </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="User" component={UserScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    flex: 1,
+    backgroundColor: 'grey',
+    justifyContent: 'center', 
+    alignItems: 'center'
+  }
+})
+
+
 
 export default main
