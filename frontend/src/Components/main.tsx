cc1f8d483249b4d4a7377726c985ce6c71e3daa9
김종관 <colinder@naver.com>
2020-11-02 21:49:12 +0900

    :heavy_plus_sign: / UPDATE icon setting  /

--- frontend/src/Components/main.tsx
+++ frontend/src/Components/main.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/dist/Ionicons';
 import HomeScreen from './Screens/home'
 import UserScreen from './Screens/user'
 
