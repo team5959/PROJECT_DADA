116c7f79a9e01bfb23413bede09620f10ef2b553
Hyejin Park <triaina0@gmail.com>
2020-11-03 14:28:21 +0900

    :hammer: / Fix *.ts to satisfy lint

--- sls-dada/feed/create.ts
+++ sls-dada/feed/create.ts
@@ -1,46 +1,45 @@
-'use strict';
+'use strict'
 
-import * as uuid from 'uuid';
-import {DynamoDB} from 'aws-sdk';
+import { DynamoDB } from 'aws-sdk'
 
-const dynamoDb = new DynamoDB.DocumentClient();
+const dynamoDb = new DynamoDB.DocumentClient()
 
 module.exports.create = (event, context, callback) => {
-    const data = JSON.parse(event.body);
+  const data = JSON.parse(event.body)
 
-    const tags = dynamoDb.createSet(data.tags);
-    delete data['tags'];
+  const tags = dynamoDb.createSet(data.tags)
+  delete data['tags']
 
-    const photos = data.photos;
-    photos.forEach(item => {
-        item.tags = dynamoDb.createSet(item.tags);
-    })
-    delete data['photos'];
+  const photos = data.photos
+  photos.forEach(item => {
+    item.tags = dynamoDb.createSet(item.tags)
+  })
+  delete data['photos']
 
-    const params = {
-        TableName: 'feed',
-        Item: {
-            tags: tags,
-            photos: photos,
-            ...data,
-        }
-    };
+  const params = {
+    TableName: 'feed',
+    Item: {
+      tags: tags,
+      photos: photos,
+      ...data
+    }
+  }
 
-    dynamoDb.put(params, (error, result) => {
-        if (error) {
-            console.error(error);
-            callback(null, {
-                statusCode: error.statusCode || 501,
-                headers: {'Content-Type': 'text/plain'},
-                body: 'Couldn\'t fetch the todo items.',
-            });
-            return;
-        }
+  dynamoDb.put(params, (error, result) => {
+    if (error) {
+      console.error(error)
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todo items.'
+      })
+      return
+    }
 
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(params.Item),
-        };
-        callback(null, response);
-    });
-};
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item)
+    }
+    callback(null, response)
+  })
+}
--- sls-dada/feed/get.ts
+++ sls-dada/feed/get.ts
@@ -1,33 +1,33 @@
-'use strict';
+'use strict'
 
-import {DynamoDB} from 'aws-sdk';
+import { DynamoDB } from 'aws-sdk'
 
-const dynamoDb = new DynamoDB.DocumentClient();
+const dynamoDb = new DynamoDB.DocumentClient()
 
 module.exports.get = (event, context, callback) => {
-    const params = {
-        TableName: 'feed',
-        Key: {
-            user: event.pathParameters.user,
-            date: event.pathParameters.date
-        },
-    };
+  const params = {
+    TableName: 'feed',
+    Key: {
+      user: event.pathParameters.user,
+      date: event.pathParameters.date
+    }
+  }
 
-    dynamoDb.get(params, (error, result) => {
-        if (error) {
-            console.error(error);
-            callback(null, {
-                statusCode: error.statusCode || 501,
-                headers: {'Content-Type': 'text/plain'},
-                body: 'Couldn\'t fetch the todo items.',
-            });
-            return;
-        }
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.error(error)
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todo items.'
+      })
+      return
+    }
 
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(result.Item),
-        };
-        callback(null, response);
-    });
-};
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item)
+    }
+    callback(null, response)
+  })
+}
