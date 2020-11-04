32a50ce05f4ce4319a5918ea98481c3e69a1ab18
Hyejin Park <triaina0@gmail.com>
2020-11-04 13:47:59 +0900

    :new: / Add deletePhoto func / S03P31A509-24

---
+++ sls-dada/photo/delete.ts
@@ -0,0 +1,36 @@
+'use strict'
+
+import { DynamoDB } from 'aws-sdk'
+
+const dynamoDb = new DynamoDB.DocumentClient()
+
+module.exports.delete = (event, context, callback) => {
+  // TODO Delete related photo in S3, tag-image in RDS
+  let params = {
+    TableName: 'feed',
+    Key: {
+      user: event.pathParameters.user,
+      date: event.pathParameters.date
+    },
+    UpdateExpression: `REMOVE photos[${event.pathParameters.photoIndex}]`,
+    ReturnValues: 'ALL_NEW'
+  }
+
+  dynamoDb.update(params, (error, result) => {
+    if (error) {
+      console.error(error)
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todo items.'
+      })
+      return
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Attributes)
+    }
+    callback(null, response)
+  })
+}
