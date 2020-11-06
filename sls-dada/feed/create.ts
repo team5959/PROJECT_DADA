5a7395156664aca85f3f8520fdcf99b6be0bcf86
Hyejin Park <triaina0@gmail.com>
2020-11-06 11:14:36 +0900

    :construction: / Add parse form-data to createFeed / S03P31A509-34

--- sls-dada/feed/create.ts
+++ sls-dada/feed/create.ts
@@ -1,26 +1,61 @@
 'use strict'
 
 import { DynamoDB } from 'aws-sdk'
+import * as stream from 'stream'
+import * as multiparty from 'multiparty'
 
 const dynamoDb = new DynamoDB.DocumentClient()
 
 module.exports.create = (event, context, callback) => {
-  const data = JSON.parse(event.body)
+  // Content-type: multipart/form-data
+  const form = new multiparty.Form()
 
-  const tags = dynamoDb.createSet(data.tags)
-  delete data['tags']
+  let str = new stream.Readable()
+  str.push(event.body)
 
-  const photos = data.photos
-  photos.forEach(item => {
-    item.tags = dynamoDb.createSet(item.tags)
+  // multyparty needs lowercase header key
+  const header = {}
+  const keys = Object.keys(event.headers)
+  for (let n = keys.length - 1; n >= 0; n--) {
+    header[keys[n].toLowerCase()] = event.headers[keys[n]]
+  }
+  str['headers'] = header
+
+  str.push(null)
+
+  form.parse(str, (err, fields, files) => {
+    if (err) {
+      console.error(err)
+      callback(null, {
+        statusCode: err.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch image files.'
+      })
+    }
+
+    console.log(files) // 사진 파일들
   })
-  delete data['photos']
+
+  // TODO get metadata from photos and divide to feeds (by location)
+
+  // TODO for all feeds
+  const data = {
+    user: event.pathParameters.user
+  }
+
+  // TODO add photos (Date, S3Object, Tags)
+
+  // TODO add S3Object (대표사진) : Earliest photo's S3Object
+  // TODO add date : Earliest photo's date
+
+  // TODO add location
+
+  // TODO add title : n월 n일의 추억
+  // TODO add tags : from photos.tags
 
   const params = {
     TableName: 'feed',
     Item: {
-      tags: tags,
-      photos: photos,
       ...data
     }
   }
@@ -31,7 +66,7 @@ module.exports.create = (event, context, callback) => {
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the todo items.'
+        body: 'Couldn\'t upload to database'
       })
       return
     }
