1f5e64652066354afc1795ba75d631f3b94203a7
김종관 <colinder@naver.com>
2020-11-08 23:20:26 +0900

    :new: / ADD page Tree setting /

--- frontend/src/Components/Screens/Feed/feedList.tsx
+++ frontend/src/Components/Screens/Feed/feedList.tsx
@@ -1,103 +1,133 @@
 import 'react-native-gesture-handler';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native'
 import { NavigationState } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import CalendarStrip from 'react-native-slideable-calendar-strip';
-import { Icon, Tile } from 'react-native-elements'
+import { Button, Icon, Tile } from 'react-native-elements'
+import { color } from 'react-native-reanimated';
 
 const Stack = createStackNavigator();
 
 interface Props {
-  navigation: NavigationState
+  navigation: NavigationState 
 }
 
 const HomeScreen = ({ navigation }: Props) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [isData, setIsData] = useState(false);
+  
+  console.log('selectedDate:', selectedDate)
 
-  return (
-    <View style={{
-      flex: 1,
-      backgroundColor: 'ivory',
-    }}>
-      <View style={{ backgroundColor: 'white'}}>
-        <CalendarStrip
-          // showWeekNumber
-          style={{ backgroundColor: 'red' }}
-          selectedDate={selectedDate}
-          onPressDate={(date) => {
-            console.log('date:',date)
-            setSelectedDate(date);
-          }}
-          // onPressGoToday={(today) => {
-          //   setSelectedDate('today:',today);
-          // }}
-          // onSwipeDown={() => {
-          //   alert('onSwipeDown');
-          // }}
-          markedDate={['2020-11-02', '2020-11-03' ]}
-          weekStartsOn={1} // 0,1,2,3,4,5,6 for S M T W T F S, defaults to 0
-        />
+
+  const getMoviesFromApi = () => {
+    return fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        return json.movies;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  if (isData) {
+    return (
+
+      <View style={{
+        flex: 1,
+        backgroundColor: 'ivory',
+      }}>
+        <View style={{ backgroundColor: 'white' }}>
+          <Button
+            title="ddd"
+            onPress={() => {
+              getMoviesFromApi()
+            }} />
+          <CalendarStrip
+            // showWeekNumber
+            selectedDate={selectedDate}
+            onPressDate={(date: React.SetStateAction<Date>) => {
+              console.log('date:', date)
+              setSelectedDate(date);
+              console.log('selectedDate:', selectedDate)
+            }}
+            // onPressGoToday={(today) => {
+            //   setSelectedDate('today:',today);
+            // }}
+            // onSwipeDown={() => {
+            //   alert('onSwipeDown');
+            // }}
+            markedDate={['2020-11-02', '2020-11-03']}
+            weekStartsOn={1} // 0,1,2,3,4,5,6 for S M T W T F S, defaults to 0
+          />
+        </View>
+
+        <View style={styles.noneContent}>
+          <Text>{JSON.stringify(selectedDate).slice(1, 11)}</Text>
+        </View>
+
+
+        <ScrollView style={{ flex: 1, backgroundColor: 'white' }}>
+          <Tile
+            height={160}
+            overlayContainerStyle={{ backgroundColor: '#aeae', borderRadius: 18, marginBottom: 10 }}
+            imageContainerStyle={{ backgroundColor: '#f7f7', borderRadius: 18 }}
+            imageSrc={require('../../../Assets/Image/test_00.png')}
+            title='2020-11-11의 추억'
+            featured
+          />
+
+          <Tile
+            height={160}
+            overlayContainerStyle={{ backgroundColor: '#a1a1a1', borderRadius: 18, marginBottom: 10 }}
+            imageContainerStyle={{ backgroundColor: '#DAD9FF', borderRadius: 18 }}
+            imageSrc={require('../../../Assets/Image/test_01.png')}
+            title='2020-11-11의 추억'
+            featured
+          />
+        </ScrollView>
+
+        <View style={styles.addFeed}>
+          <Icon
+            reverse
+            name='add'
+            type='ionicon'
+            color='#f261DF'
+            size={20}
+            reverseColor='black'
+            onPress={() => {
+              console.log('사진 선택시작')
+              navigation.navigate('photoSelectFeed')
+            }} />
+        </View>
       </View>
-      
-      
-      <View style={styles.noneContent}>
-        <Text>작성된 피드가 없으면 이걸 보여줍니다.</Text>
+    )
+  } else {
+    return (
+      <View>
+        <Text>자료가 없어면 보여줄 화면</Text>
+        <TouchableOpacity>
+          <Image source={require('../../../Assets/Image/test_00.png')} />
+        </TouchableOpacity>
+        
       </View>
-      
-
-
-
-      <Tile
-        featured
-        imageContainerStyle={{ 
-          borderRadius: 20,
-          // backgroundColor: '#f434', 
-        }}
-        imageSrc={require('../../../Assets/Image/test_00.png')}
-        title="2020-11-03의 일기입니다."
-        height={150}
-        caption="Some Caption Text"
-        captionStyle={{
-          backgroundColor: 'red'
-        }}
-      >;
-      </Tile> 
-
-      
-
-      
-      
-      <View style={styles.addFeed}>
-        <Icon
-          reverse
-          name='add'
-          type='ionicon'
-          color='#543fa4'
-          size={20}
-          reverseColor='white'
-          onPress={()=>{
-            console.log('사진 선택시작')
-            navigation.navigate('photoSelectFeed')  
-          }} />
-      </View>
-      
-    </View>
-  )
+    )
+  }
+  
 }
 const styles = StyleSheet.create({
+
   addFeed: {
-    flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
   },
-  addIcon: {
-    backgroundColor: "white"
-  },
   noneContent: {
-    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginTop: 3,
+    marginBottom: 3,
   }
 
 })
