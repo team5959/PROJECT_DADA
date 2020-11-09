463a6b2fa9e2a5d70c7f6643515177b13c0a1e99
Hyejin Park <triaina0@gmail.com>
2020-11-09 23:05:36 +0900

    :construction: / Add S3 handler

---
+++ sls-dada/lib/S3Handler.ts
@@ -0,0 +1,17 @@
+// reference : https://github.com/serverless/examples/blob/master/aws-node-dynamic-image-resizer/src/handlers/resizer/s3Handler.js
+import { S3 } from 'aws-sdk'
+
+const s3 = new S3()
+
+export async function readFileFromS3 ({ Bucket, Key }: { Bucket: string, Key: string }) {
+  return new Promise((resolve, reject) => {
+    s3.getObject({ Bucket, Key }, (err, data) => {
+      if (err) {
+        console.log(err)
+        return reject(err)
+      }
+
+      return resolve(data.Body)
+    })
+  })
+}
