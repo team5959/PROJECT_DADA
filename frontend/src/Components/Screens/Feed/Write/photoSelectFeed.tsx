66236cfaf2787b7895e8547e2e8613e51131115c
SW Certificate <hunmin8788@naver.com>
2020-11-09 17:05:23 +0900

    ADD / image-crop-picker success

--- frontend/src/Components/Screens/Feed/Write/photoSelectFeed.tsx
+++ frontend/src/Components/Screens/Feed/Write/photoSelectFeed.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { View, Text, Button, StyleSheet} from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet, BackHandler} from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, NavigationState } from '@react-navigation/native';
-
+import CropPicker from 'react-native-image-crop-picker';
+import { keyframes } from 'styled-components';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -13,6 +14,25 @@ interface Props {
 }
 
 const createFeed = ({ navigation }: Props) => {
+
+  const tmp = new FormData();
+
+  useEffect (() => {
+    CropPicker.openPicker({
+      multiple: true,
+      mediaType: 'photo',
+      inculdeExif: true
+    }).then(images => {
+      console.log(images);
+      for(var i = 0; i < images.length; i ++){
+        tmp.append('path',images[i].path);
+      }
+    }).catch(() => {
+      console.log('사진을 불러오는데 실패하였습니다');
+      navigation.navigate('Feed')
+    })
+  })
+
   return (
     <View>
       <Text>피드 생성을 위한 사진 선택창입니다.</Text>   
@@ -21,12 +41,12 @@ const createFeed = ({ navigation }: Props) => {
         title='피드 내용 달기'
         onPress={() => {
           console.log('수정 페이지이동')
+          console.log("불러온 데이터", tmp)
           navigation.navigate('EditFeed')
         }} />
     </View>
   )
 }
-
 
 // const Styles = StyleSheet.create ({
 //   feed: {
