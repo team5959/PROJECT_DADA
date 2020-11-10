3c807b5f1226fb64bd814dd2fcf477a0ba9ccb97
Hyejin Park <triaina0@gmail.com>
2020-11-10 14:51:37 +0900

    :construction: / Add address to photo
    using gps info of photo

--- sls-dada/feed/create.ts
+++ sls-dada/feed/create.ts
@@ -3,6 +3,7 @@
 import { DynamoDB, S3 } from 'aws-sdk'
 import * as exif from 'exif-parser'
 import { readFileFromS3 } from '../lib/S3Handler'
+import { getAddress } from '../lib/KakaoAPIHandler'
 
 const dynamoDb = new DynamoDB.DocumentClient()
 
@@ -27,41 +28,49 @@ interface Feed {
   photos: Array<Photo>
 }
 
-async function groupingFeeds (s3Objects: Array<S3Object>): Promise<Array<Feed>> {
-  const feeds: Array<Feed> = []
+async function extractData (s3Objects: Array<S3Object>): Promise<Array<Photo>> {
+  const photos = []
 
-  let tmpAddr: string = ''
   for (const o of s3Objects) {
+    const photo = {
+      S3Object: o
+    }
     const buffer = await readFileFromS3(o)
     const parser = exif.create(buffer)
     try {
       const result = parser.parse()
-      // now we can use gps from result
-      // -> result.tags.GPSLatitude, result.tags.GPSLongitude (double type)
-      // ; if it's not exist, not use this photo
-      // maybe we should check that photos from gallery (app) have gps information (I have to manual export from mac photos app to get gps info)
 
       // maybe it's the original timestamp...
       // result.tags.DateTimeOriginal or result.tags.CreateDate (long type -> need to casting)
+
+      if ('GPSLongitude' in result.tags) {
+        photo['gps'] = {
+          lng: result.tags['GPSLongitude'],
+          lat: result.tags['GPSLatitude']
+        }
+        photo['location'] = await getAddress(photo['gps'])
+      }
     } catch (err) {
-      console.error(`Image file ${o.Bucket}/${o.Key} dosen't have gps info... skipped`)
+      console.error(`Image file ${o.Bucket}/${o.Key} doesn't have meta data... skipped`)
     }
+
+    photos.push(photo)
   }
 
-  return feeds
+  return photos
 }
 
 module.exports.create = async (event, context, callback) => {
   const data = JSON.parse(event.body)
 
   // TODO divide to feeds (by location)
-  const feeds = await groupingFeeds(data['photos'])
+  const photos = await extractData(data['photos'])
   // console.log(feeds)
 
-  // // TODO for all feeds
-  // const item = {
-  //   user: event.pathParameters.user
-  // }
+  const item = {
+    user: event.pathParameters.user,
+    photos: photos
+  }
   //
   // // TODO add photos (Date, S3Object, Tags)
   //
