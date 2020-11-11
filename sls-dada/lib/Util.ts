8d56961223dd210222b4e9647b7fa22187aa8345
Hyejin Park <triaina0@gmail.com>
2020-11-11 12:49:08 +0900

    :bug: / Change date format to UTC
    KST shows time after 9 hour

--- sls-dada/lib/Util.ts
+++ sls-dada/lib/Util.ts
@@ -2,5 +2,5 @@ import * as moment from 'moment-timezone'
 
 export const convertToDate = (timestamp: number): string => {
   const d = moment(timestamp * 1000).tz('Asia/Seoul')
-  return d.format('YYYY-MM-DDTHH:mm:ss')
+  return d.utc().format('YYYY-MM-DDTHH:mm:ss')
 }
